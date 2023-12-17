4a638fa9fb3d1d1dcc83dc336591782cd7465754
news1d <95386626+news1d@users.noreply.github.com>
2023-12-17 05:35:05 +0300

    Update main.cpp
    Added initial values

--- main.cpp
+++ main.cpp
@@ -12,6 +12,21 @@ double recur(vector<vector<double>>& C, double dT, double h, int n, int j) {
 }
 
 int main(int argc, char** argv) {
+    MPI_Init(&argc, &argv);
+
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    double dT = 0.1;
+    double h = 0.1;
+    double x_0 = 0;
+    double x_n = 1;
+    double t_0 = 0;
+    double t_n = 1;
+
+    int x_nums = int((x_n - x_0) / h);
+    int N = int((t_n - t_0) / dT);
 
 
     return 0;
